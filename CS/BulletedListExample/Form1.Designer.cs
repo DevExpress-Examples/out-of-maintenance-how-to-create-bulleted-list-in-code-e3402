c63344d0540005395f4dd6d17d87917a9328d3ee
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/BulletedListExample/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace BulletedListExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richEditControl1 = new DevExpress.XtraRichEdit.RichEditControl();
+            this.btnSimpleBullet = new System.Windows.Forms.Button();
+            this.comboBoxEdit1 = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.comboBoxEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richEditControl1
+            // 
+            this.richEditControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.richEditControl1.Location = new System.Drawing.Point(0, 0);
+            this.richEditControl1.Name = "richEditControl1";
+            this.richEditControl1.Options.Fields.UseCurrentCultureDateTimeFormat = false;
+            this.richEditControl1.Options.MailMerge.KeepLastParagraph = false;
+            this.richEditControl1.Size = new System.Drawing.Size(532, 562);
+            this.richEditControl1.TabIndex = 0;
+            this.richEditControl1.Text = "richEditControl1";
+            // 
+            // btnSimpleBullet
+            // 
+            this.btnSimpleBullet.Location = new System.Drawing.Point(550, 12);
+            this.btnSimpleBullet.Name = "btnSimpleBullet";
+            this.btnSimpleBullet.Size = new System.Drawing.Size(102, 23);
+            this.btnSimpleBullet.TabIndex = 1;
+            this.btnSimpleBullet.Text = "Create bulleted list";
+            this.btnSimpleBullet.UseVisualStyleBackColor = true;
+            this.btnSimpleBullet.Click += new System.EventHandler(this.btnSimpleBullet_Click);
+            // 
+            // comboBoxEdit1
+            // 
+            this.comboBoxEdit1.EditValue = ((short)(0));
+            this.comboBoxEdit1.Location = new System.Drawing.Point(620, 75);
+            this.comboBoxEdit1.Name = "comboBoxEdit1";
+            this.comboBoxEdit1.Properties.AllowNullInput = DevExpress.Utils.DefaultBoolean.False;
+            this.comboBoxEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.comboBoxEdit1.Properties.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2"});
+            this.comboBoxEdit1.Properties.TextEditStyle = DevExpress.XtraEditors.Controls.TextEditStyles.HideTextEditor;
+            this.comboBoxEdit1.Size = new System.Drawing.Size(30, 18);
+            this.comboBoxEdit1.TabIndex = 2;
+            this.comboBoxEdit1.SelectedIndexChanged += new System.EventHandler(this.comboBoxEdit1_SelectedIndexChanged);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(550, 80);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(67, 13);
+            this.labelControl1.TabIndex = 3;
+            this.labelControl1.Text = "Set List Level:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 562);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.comboBoxEdit1);
+            this.Controls.Add(this.btnSimpleBullet);
+            this.Controls.Add(this.richEditControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.comboBoxEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraRichEdit.RichEditControl richEditControl1;
+        private System.Windows.Forms.Button btnSimpleBullet;
+        private DevExpress.XtraEditors.ComboBoxEdit comboBoxEdit1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+    }
+}
+
---
+++ Readme.md
@@ -0,0 +1,12 @@
+# How to create bulleted list in code
+
+
+<p>This example illustrates how to create standard bulleted list in code. </p><p>First we add a new <a href="http://documentation.devexpress.com/#CoreLibraries/clsDevExpressXtraRichEditAPINativeAbstractNumberingListtopic"><u>AbstractNumberingList</u></a> to the <a href="http://documentation.devexpress.com/#CoreLibraries/DevExpressXtraRichEditAPINativeDocument_AbstractNumberingListstopic"><u>Document.AbstractNumberingLists</u></a> collection.<br />
+Then we specify a <a href="http://documentation.devexpress.com/#CoreLibraries/DevExpressXtraRichEditAPINativeNumberingListBase_NumberingTypetopic"><u>NumberingType</u></a> property so it equals the <strong>NumberingType.Bullet</strong> value.<br />
+For each <a href="http://documentation.devexpress.com/#CoreLibraries/clsDevExpressXtraRichEditAPINativeListLeveltopic"><u>ListLevel</u></a> we specify the paragraph indentation, picture numbering symbol and font. The <a href="http://documentation.devexpress.com/#CoreLibraries/DevExpressXtraRichEditAPINativeListLevelProperties_NumberingFormattopic"><u>NumberingFormat</u></a> should be set to <strong>NumberingFormat.Bullet</strong>.<br />
+Create a numbering list definition that will be applied to the paragraphs in the document. To accomplish this, use the <strong>Add </strong>method of the <a href="http://documentation.devexpress.com/#CoreLibraries/DevExpressXtraRichEditAPINativeDocument_NumberingListstopic"><u>NumberingList collection.</u></a> The parameter is the index of an abstract numbering list created before.<br />
+To include a paragraph in a bulleted list, set the <a href="http://documentation.devexpress.com/#CoreLibraries/DevExpressXtraRichEditAPINativeParagraph_ListIndextopic"><u>ListIndex</u></a> property of a paragraph to the index of a list in the document and specify the <a href="http://documentation.devexpress.com/#CoreLibraries/DevExpressXtraRichEditAPINativeParagraph_ListLeveltopic"><u>ListLevel</u></a> property.</p>
+
+<br/>
+
+
